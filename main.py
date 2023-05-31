12c9341f89d8444924f4866de100845479a5ce47
Walter Moura <walter.moura@sleyersx.com.br>
2023-05-31 11:21:23 -0300

    Update main

--- main.py
+++ main.py
@@ -1,93 +1,38 @@
 import os
-import shutil
-import datetime
-import locale
 import json
-
-def delete_directory(path):
-    shutil.rmtree(path)
-
-def create_folders(root_path):
-    # Obter a data atual
-    now = datetime.datetime.now()
-
-    # Definir a configuração para o idioma brasileiro
-    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-
-    # Formatar a data para o nome da pasta raiz
-    folder_name = now.strftime("%A %d DE %B %Y").upper()
-
-    # Verificar se a pasta raiz já existe
-    root_folder_path = os.path.join(root_path, folder_name)
-
-    if os.path.exists(root_folder_path):
-        # Pasta raiz já existe, solicitar confirmação do usuário
-        confirm = input("A pasta raiz já existe. Deseja sobrescrevê-la? (S/N): ")
-
-        if confirm.upper() != "S":
-            # O usuário não confirmou, encerrar o programa
-            print("Programa encerrado.")
-            return
-
-        delete_directory(root_folder_path)
-
-    # Criar a pasta raiz
-    os.makedirs(root_folder_path)
-
-    # Criar as subpastas
-    subfolders = [
-        "01 - ESCOLA SABATINA",
-        "02 - ANÚNCIOS",
-        "03 - SAÚDE",
-        "04 - PROVAI E VEDE",
-        "05 - MENSAGEM MUSICAL",
-        "06 - CULTO DIVINO",
-        "07 - FUNDO MUSICAL"
-    ]
-
-    for subfolder in subfolders:
-        subfolder_path = os.path.join(root_folder_path, subfolder)
-
-        # Verificar se a subpasta já existe e removê-la se necessário
-        if os.path.exists(subfolder_path):
-            delete_directory(subfolder_path)
-
-        os.makedirs(subfolder_path)
+import shutil
 
 def copy_files(source_path, destination_path):
     shutil.copy(source_path, destination_path)
 
-def copy_config_files(config_file_path, destination_path):
-    with open(config_file_path, "r") as file:
-        files_to_copy = json.load(file)
+def copy_config_files(root_path):
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
 
-    for file_info in files_to_copy:
-        source_file_path = file_info["source"]
-        destination_file_path = os.path.join(destination_path, os.path.basename(source_file_path))
+    with open(config_path) as file:
+        config = json.load(file)
 
-        copy_files(source_file_path, destination_file_path)
+    for entry in config:
+        source_path = entry['source']
+        destination_path = os.path.join(root_path, entry['destination'])
+
+        copy_files(source_path, destination_path)
 
 def main():
-    root_path = "C:\\Users\\walte\\Desktop"
-    config_file_path = os.path.join(os.path.dirname(__file__), "config.json")
-
-    create_folders(root_path)
-
-    # Obter a data atual
+    # Obtenha a data atual
+    import datetime
     now = datetime.datetime.now()
+    formatted_date = now.strftime('%A %d DE %B %Y').upper()
 
-    # Definir a configuração para o idioma brasileiro
-    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+    # Defina o caminho da pasta raiz
+    root_path = os.path.join('C:\\Users\\walte\\Desktop', formatted_date)
 
-    # Formatar a data para o nome da pasta raiz
-    folder_name = now.strftime("%A %d DE %B %Y").upper()
+    # Crie as pastas
+    os.makedirs(root_path, exist_ok=True)
 
-    # Caminho completo da pasta raiz
-    root_folder_path = os.path.join(root_path, folder_name)
+    # Copie os arquivos de configuração
+    copy_config_files(root_path)
 
-    copy_config_files(config_file_path, root_folder_path)
+    print('Estrutura de pastas criada com sucesso!')
 
-    print("Estrutura de pastas criada com sucesso!")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
