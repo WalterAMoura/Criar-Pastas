f06ee7348aa30d207cd230f38f869dd569823205
Walter Moura <walter.moura@sleyersx.com.br>
2023-05-31 11:09:22 -0300

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import os
 import shutil
 import datetime
+import locale
+
+def delete_directory(path):
+    shutil.rmtree(path)
 
 def create_folders(root_path):
+    # Obter a data atual
+    now = datetime.datetime.now()
+
+    # Definir a configuração para o idioma brasileiro
+    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+
+    # Formatar a data para o nome da pasta raiz
+    folder_name = now.strftime("%A %d DE %B %Y").upper()
+
+    # Verificar se a pasta raiz já existe
+    root_folder_path = os.path.join(root_path, folder_name)
+
+    if os.path.exists(root_folder_path):
+        # Pasta raiz já existe, solicitar confirmação do usuário
+        confirm = input("A pasta raiz já existe. Deseja sobrescrevê-la? (S/N): ")
+
+        if confirm.upper() != "S":
+            # O usuário não confirmou, encerrar o programa
+            print("Programa encerrado.")
+            return
+
+        delete_directory(root_folder_path)
+
+    # Criar a pasta raiz
+    os.makedirs(root_folder_path)
+
     # Criar as subpastas
     subfolders = [
         "01 - ESCOLA SABATINA",
@@ -15,11 +45,11 @@ def create_folders(root_path):
     ]
 
     for subfolder in subfolders:
-        subfolder_path = os.path.join(root_path, subfolder)
+        subfolder_path = os.path.join(root_folder_path, subfolder)
 
         # Verificar se a subpasta já existe e removê-la se necessário
         if os.path.exists(subfolder_path):
-            shutil.rmtree(subfolder_path)
+            delete_directory(subfolder_path)
 
         os.makedirs(subfolder_path)
 
@@ -30,7 +60,7 @@ def copy_config_files(source_path, destination_path):
     files_to_copy = [
         {"source": "file1.txt", "destination": "01 - ESCOLA SABATINA/file1.txt"},
         {"source": "file2.txt", "destination": "04 - PROVAI E VEDE/file2.txt"},
-        {"source": "file3.txt", "destination": "06 - CULTO DIVINO/file3.txt"}
+        {"source": "file3.txt", "destination": "07 - FUNDO MUSICAL/file3.txt"}
     ]
 
     for file_info in files_to_copy:
@@ -43,28 +73,23 @@ def main():
     root_path = "C:\\Users\\walte\\Desktop"
     source_path = "E:\\Projetos\\Igreja\\pastas-copy-paste"
 
-    # Criar a pasta raiz com base na data atual
+    create_folders(root_path)
+
+    # Obter a data atual
     now = datetime.datetime.now()
-    folder_name = now.strftime("%Y-%m-%d")
-    destination_path = os.path.join(root_path, folder_name)
 
-    if os.path.exists(destination_path):
-        # Pasta raiz já existe, solicitar confirmação do usuário
-        confirm = input("A pasta raiz já existe. Deseja sobrescrevê-la? (S/N): ")
+    # Definir a configuração para o idioma brasileiro
+    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
 
-        if confirm.upper() != "S":
-            # O usuário não confirmou, encerrar o programa
-            print("Programa encerrado.")
-            return
+    # Formatar a data para o nome da pasta raiz
+    folder_name = now.strftime("%A %d DE %B %Y").upper()
 
-        shutil.rmtree(destination_path)
+    # Caminho completo da pasta raiz
+    root_folder_path = os.path.join(root_path, folder_name)
 
-    os.makedirs(destination_path)
+    copy_config_files(source_path, root_folder_path)
 
-    create_folders(destination_path)
-    copy_config_files(source_path, destination_path)
+    print("Estrutura de pastas criada com sucesso!")
 
 if __name__ == "__main__":
     main()
-
-
