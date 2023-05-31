860a7d739fa6b25605e7671f52d3a9205dbdfb5b
Walter Moura <walter.moura@sleyersx.com.br>
2023-05-31 18:14:11 -0300

    Update main

--- main.py
+++ main.py
@@ -8,62 +8,50 @@ import locale
 def copy_files(source_path, destination_path):
     try:
         shutil.copy2(source_path, destination_path)
-        print(f'Arquivo copiado: {source_path} -> {destination_path}')
-    except FileNotFoundError:
-        print(f'Erro ao copiar arquivo: {source_path} -> {destination_path}')
+        print(f'Arquivo {source_path} copiado para {destination_path}')
+    except Exception as e:
+        print(f'Erro ao copiar o arquivo {source_path}: {str(e)}')
 
-def copy_config_files(root_path):
-    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-
-    with open(config_path) as file:
-        config = json.load(file)
-
+def copy_config_files(root_path, config):
     for entry in config['filesCopy']:
-        source_path = os.path.join(config['rootPath'], entry['source'])
-        destination_path = os.path.join(root_path, entry['destination'])
+        source_path = os.path.join(entry['source'], entry['fileName'])
+        destination_path = os.path.join(root_path, entry['destination'], entry['fileName'])
 
         copy_files(source_path, destination_path)
 
-def create_subfolders(root_path):
-    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-
-    with open(config_path) as file:
-        config = json.load(file)
-
-    for subfolder in config['subFolders']:
+def create_subfolders(root_path, subfolders):
+    for subfolder in subfolders:
         subfolder_path = os.path.join(root_path, subfolder)
         os.makedirs(subfolder_path, exist_ok=True)
 
 def main():
-    # Defina o idioma para o formato de data em português
+    # Define o idioma para o formato de data em português
     locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
 
-    # Obtenha a data atual
+    # Obtém a data atual
     now = datetime.now()
     formatted_date = now.strftime('%A %d DE %B %Y').upper()
 
-    # Verifique se o dia da semana está na lista de dias de execução
-    weekday = now.strftime('%a').lower()
+    # Lê as configurações do arquivo config.json
     config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-
     with open(config_path) as file:
         config = json.load(file)
 
-    if weekday not in config['dayOfExecution']:
-        print('O script não está configurado para ser executado hoje.')
-        return
+    # Obtém o caminho da pasta raiz a partir do arquivo de configuração
+    root_path = config['rootPath']
+    root_path = os.path.join(root_path, formatted_date)
 
-    # Crie a pasta raiz com a data formatada
-    root_path = os.path.join(config['rootPath'], formatted_date)
-
-    # Remova a acentuação do nome da pasta raiz
+    # Remove a acentuação do nome da pasta raiz
     root_path_normalized = unidecode(root_path)
 
-    # Crie as subpastas
-    create_subfolders(root_path_normalized)
+    # Obtém a lista de subpastas a partir do arquivo de configuração
+    subfolders = config['subFolders']
 
-    # Copie os arquivos de configuração
-    copy_config_files(root_path_normalized)
+    # Cria as subpastas
+    create_subfolders(root_path_normalized, subfolders)
+
+    # Copia os arquivos de configuração
+    copy_config_files(root_path_normalized, config)
 
     print('Estrutura de pastas criada com sucesso!')
 
