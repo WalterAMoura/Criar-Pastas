0a51d2ed5160afe63843e3be2b2e5068e5a437b6
Walter Moura <walter.moura@sleyersx.com.br>
2023-06-01 16:24:05 -0300

    Update main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import json
 import shutil
 from datetime import datetime
 from unidecode import unidecode
-import locale
 import re
+import locale
 
 def copy_files(source_path, destination_path):
     try:
@@ -13,77 +13,90 @@ def copy_files(source_path, destination_path):
     except Exception as e:
         print(f'Erro ao copiar o arquivo {source_path}: {str(e)}')
 
-def copy_config_files(root_path):
-    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-
-    with open(config_path) as file:
-        config = json.load(file)
-
+def copy_config_files(root_path, config):
     for entry in config['filesCopy']:
-        source_path = entry['source']
-        destination_path = os.path.join(root_path, entry['destination'])
+        source_path = format_path(entry['source'], config)
+        destination_path = format_path(entry['destination'], config)
+        file_name = format_file_name(entry['fileName'], config)
 
-        # Obtenha o nome do arquivo original
-        file_name = entry['fileName']
+        source_files = find_files_with_regex(source_path, file_name)
+        for file_path in source_files:
+            dest_file_path = os.path.join(root_path, destination_path, os.path.basename(file_path))
+            copy_files(file_path, dest_file_path)
 
-        # Substitua os placeholders no nome do arquivo usando regex
-        placeholders = re.findall(r'\{\{(.*?)\}\}', file_name)
-        for placeholder in placeholders:
-            if placeholder == 'dia':
-                # Obtenha o dia atual
-                dia_atual = datetime.now().strftime('%d')
-                file_name = file_name.replace('{{dia}}', dia_atual)
-            elif placeholder == 'ano':
-                # Obtenha o ano atual
-                ano_atual = datetime.now().strftime('%Y')
-                file_name = file_name.replace('{{ano}}', ano_atual)
-            elif placeholder == 'numeroMes':
-                # Obtenha o número do mês atual
-                numero_mes_atual = datetime.now().strftime('%m')
-                file_name = file_name.replace('{{numeroMes}}', numero_mes_atual)
-            elif placeholder == 'nomeMes':
-                # Obtenha o nome do mês atual
-                nome_mes_atual = datetime.now().strftime('%B')
-                file_name = file_name.replace('{{nomeMes}}', nome_mes_atual.upper())
-
-        # Copie o arquivo apenas se corresponder ao padrão do regex
-        if re.match(file_name, os.path.basename(source_path)):
-            copy_files(source_path, destination_path)
-
-def create_subfolders(root_path):
-    subfolders = [
-        '01 - ESCOLA SABATINA',
-        '02 - ANUNCIOS',
-        '03 - SAUDE',
-        '04 - PROVAI E VEDE',
-        '05 - MENSAGEM MUSICAL',
-        '06 - CULTO DIVINO',
-        '07 - FUNDO MUSICAL'
-    ]
-
+def create_subfolders(root_path, subfolders):
     for subfolder in subfolders:
         subfolder_path = os.path.join(root_path, subfolder)
         os.makedirs(subfolder_path, exist_ok=True)
 
+def format_path(path, config):
+    now = datetime.now()
+    placeholders = {
+        '{{ano}}': now.year,
+        '{{numeroMes}}': f'{now.month:02d}',
+        '{{nomeMes}}': now.strftime('%B'),
+        '{{dia}}': f'{now.day:02d}',
+        '{{hora}}': f'{now.hour:02d}',
+        '{{minuto}}': f'{now.minute:02d}',
+        '{{segundo}}': f'{now.second:02d}',
+    }
+    for placeholder, value in placeholders.items():
+        path = path.replace(placeholder, str(value))
+    return path
+
+def format_file_name(file_name, config):
+    now = datetime.now()
+    placeholders = {
+        '{{ano}}': now.year,
+        '{{numeroMes}}': f'{now.month:02d}',
+        '{{nomeMes}}': now.strftime('%B'),
+        '{{dia}}': f'{now.day:02d}',
+        '{{hora}}': f'{now.hour:02d}',
+        '{{minuto}}': f'{now.minute:02d}',
+        '{{segundo}}': f'{now.second:02d}',
+        '{{nomeArquivo}}': '',
+    }
+    for placeholder, value in placeholders.items():
+        file_name = file_name.replace(placeholder, str(value))
+    return file_name
+
+def find_files_with_regex(directory, pattern):
+    files = []
+    regex = re.compile(pattern)
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            if regex.match(filename):
+                files.append(os.path.join(root, filename))
+    return files
+
 def main():
-    # Defina o idioma para o formato de data em português
+    # Define o idioma para o formato de data em português
     locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
 
-    # Obtenha a data atual
+    # Obtém a data atual
     now = datetime.now()
     formatted_date = now.strftime('%A %d DE %B %Y').upper()
 
-    # Crie a pasta raiz com a data formatada
-    root_path = os.path.join('C:\\Users\\username\\Desktop', formatted_date)  # Substitua "username" pelo seu nome de usuário
+    # Lê as configurações do arquivo config.json
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+    with open(config_path) as file:
+        config = json.load(file)
 
-    # Remova a acentuação do nome da pasta raiz
+    # Obtém o caminho da pasta raiz a partir do arquivo de configuração
+    root_path = config['rootPath']
+    root_path = os.path.join(root_path, formatted_date)
+
+    # Remove a acentuação do nome da pasta raiz
     root_path_normalized = unidecode(root_path)
 
-    # Crie as subpastas
-    create_subfolders(root_path_normalized)
+    # Obtém a lista de subpastas a partir do arquivo de configuração
+    subfolders = config['subFolders']
 
-    # Copie os arquivos de configuração
-    copy_config_files(root_path_normalized)
+    # Cria as subpastas
+    create_subfolders(root_path_normalized, subfolders)
+
+    # Copia os arquivos de configuração
+    copy_config_files(root_path_normalized, config)
 
     print('Estrutura de pastas criada com sucesso!')
 
