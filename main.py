f55b9a66dd94613da4ff619474c7932bc9f2d1ec
Walter Moura <walter.moura@sleyersx.com.br>
2023-06-01 16:26:52 -0300

    Update main

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import re
 import locale
 
 def copy_files(source_path, destination_path):
+    """
+    Copia um arquivo de origem para o destino.
+
+    Args:
+        source_path (str): Caminho do arquivo de origem.
+        destination_path (str): Caminho de destino do arquivo.
+    """
     try:
         shutil.copy2(source_path, destination_path)
         print(f'Arquivo {source_path} copiado para {destination_path}')
@@ -14,6 +21,13 @@ def copy_files(source_path, destination_path):
         print(f'Erro ao copiar o arquivo {source_path}: {str(e)}')
 
 def copy_config_files(root_path, config):
+    """
+    Copia os arquivos listados no arquivo de configuração.
+
+    Args:
+        root_path (str): Caminho da pasta raiz.
+        config (dict): Configurações do script.
+    """
     for entry in config['filesCopy']:
         source_path = format_path(entry['source'], config)
         destination_path = format_path(entry['destination'], config)
@@ -25,11 +39,28 @@ def copy_config_files(root_path, config):
             copy_files(file_path, dest_file_path)
 
 def create_subfolders(root_path, subfolders):
+    """
+    Cria as subpastas no caminho especificado.
+
+    Args:
+        root_path (str): Caminho da pasta raiz.
+        subfolders (list): Lista de nomes de subpastas a serem criadas.
+    """
     for subfolder in subfolders:
         subfolder_path = os.path.join(root_path, subfolder)
         os.makedirs(subfolder_path, exist_ok=True)
 
 def format_path(path, config):
+    """
+    Formata o caminho substituindo os placeholders pelos valores correspondentes.
+
+    Args:
+        path (str): Caminho a ser formatado.
+        config (dict): Configurações do script.
+
+    Returns:
+        str: Caminho formatado.
+    """
     now = datetime.now()
     placeholders = {
         '{{ano}}': now.year,
@@ -45,6 +76,16 @@ def format_path(path, config):
     return path
 
 def format_file_name(file_name, config):
+    """
+    Formata o nome do arquivo substituindo os placeholders pelos valores correspondentes.
+
+    Args:
+        file_name (str): Nome do arquivo a ser formatado.
+        config (dict): Configurações do script.
+
+    Returns:
+        str: Nome do arquivo formatado.
+    """
     now = datetime.now()
     placeholders = {
         '{{ano}}': now.year,
@@ -54,19 +95,27 @@ def format_file_name(file_name, config):
         '{{hora}}': f'{now.hour:02d}',
         '{{minuto}}': f'{now.minute:02d}',
         '{{segundo}}': f'{now.second:02d}',
-        '{{nomeArquivo}}': '',
+        '{{nomeArquivo}}': os.path.splitext(os.path.basename(file_name))[0],
     }
     for placeholder, value in placeholders.items():
         file_name = file_name.replace(placeholder, str(value))
     return file_name
 
-def find_files_with_regex(directory, pattern):
+def find_files_with_regex(source_path, file_name_pattern):
+    """
+    Encontra os arquivos que correspondem ao padrão de nome de arquivo usando expressões regulares.
+
+    Args:
+        source_path (str): Caminho da pasta de origem.
+        file_name_pattern (str): Padrão do nome do arquivo usando expressões regulares.
+
+    Returns:
+        list: Lista de caminhos dos arquivos correspondentes.
+    """
     files = []
-    regex = re.compile(pattern)
-    for root, _, filenames in os.walk(directory):
-        for filename in filenames:
-            if regex.match(filename):
-                files.append(os.path.join(root, filename))
+    for file_name in os.listdir(source_path):
+        if re.match(file_name_pattern, file_name):
+            files.append(os.path.join(source_path, file_name))
     return files
 
 def main():
