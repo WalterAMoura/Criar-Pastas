ddf1929e977fee43b3a8a6ad08fd0218126780ab
Walter Moura <walter.moura@sleyersx.com.br>
2023-06-01 16:14:50 -0300

    Update main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil
 from datetime import datetime
 from unidecode import unidecode
 import locale
+import re
 
 def copy_files(source_path, destination_path):
     try:
@@ -12,46 +13,77 @@ def copy_files(source_path, destination_path):
     except Exception as e:
         print(f'Erro ao copiar o arquivo {source_path}: {str(e)}')
 
-def copy_config_files(root_path, config):
+def copy_config_files(root_path):
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+
+    with open(config_path) as file:
+        config = json.load(file)
+
     for entry in config['filesCopy']:
-        source_path = os.path.join(entry['source'], entry['fileName'])
-        destination_path = os.path.join(root_path, entry['destination'], entry['fileName'])
+        source_path = entry['source']
+        destination_path = os.path.join(root_path, entry['destination'])
 
-        copy_files(source_path, destination_path)
+        # Obtenha o nome do arquivo original
+        file_name = entry['fileName']
 
-def create_subfolders(root_path, subfolders):
+        # Substitua os placeholders no nome do arquivo usando regex
+        placeholders = re.findall(r'\{\{(.*?)\}\}', file_name)
+        for placeholder in placeholders:
+            if placeholder == 'dia':
+                # Obtenha o dia atual
+                dia_atual = datetime.now().strftime('%d')
+                file_name = file_name.replace('{{dia}}', dia_atual)
+            elif placeholder == 'ano':
+                # Obtenha o ano atual
+                ano_atual = datetime.now().strftime('%Y')
+                file_name = file_name.replace('{{ano}}', ano_atual)
+            elif placeholder == 'numeroMes':
+                # Obtenha o número do mês atual
+                numero_mes_atual = datetime.now().strftime('%m')
+                file_name = file_name.replace('{{numeroMes}}', numero_mes_atual)
+            elif placeholder == 'nomeMes':
+                # Obtenha o nome do mês atual
+                nome_mes_atual = datetime.now().strftime('%B')
+                file_name = file_name.replace('{{nomeMes}}', nome_mes_atual.upper())
+
+        # Copie o arquivo apenas se corresponder ao padrão do regex
+        if re.match(file_name, os.path.basename(source_path)):
+            copy_files(source_path, destination_path)
+
+def create_subfolders(root_path):
+    subfolders = [
+        '01 - ESCOLA SABATINA',
+        '02 - ANUNCIOS',
+        '03 - SAUDE',
+        '04 - PROVAI E VEDE',
+        '05 - MENSAGEM MUSICAL',
+        '06 - CULTO DIVINO',
+        '07 - FUNDO MUSICAL'
+    ]
+
     for subfolder in subfolders:
         subfolder_path = os.path.join(root_path, subfolder)
         os.makedirs(subfolder_path, exist_ok=True)
 
 def main():
-    # Define o idioma para o formato de data em português
+    # Defina o idioma para o formato de data em português
     locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
 
-    # Obtém a data atual
+    # Obtenha a data atual
     now = datetime.now()
     formatted_date = now.strftime('%A %d DE %B %Y').upper()
 
-    # Lê as configurações do arquivo config.json
-    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-    with open(config_path) as file:
-        config = json.load(file)
+    # Crie a pasta raiz com a data formatada
+    root_path = os.path.join('C:\\Users\\username\\Desktop', formatted_date)  # Substitua "username" pelo seu nome de usuário
 
-    # Obtém o caminho da pasta raiz a partir do arquivo de configuração
-    root_path = config['rootPath']
-    root_path = os.path.join(root_path, formatted_date)
-
-    # Remove a acentuação do nome da pasta raiz
+    # Remova a acentuação do nome da pasta raiz
     root_path_normalized = unidecode(root_path)
 
-    # Obtém a lista de subpastas a partir do arquivo de configuração
-    subfolders = config['subFolders']
+    # Crie as subpastas
+    create_subfolders(root_path_normalized)
 
-    # Cria as subpastas
-    create_subfolders(root_path_normalized, subfolders)
-
-    # Copia os arquivos de configuração
-    copy_config_files(root_path_normalized, config)
+    # Copie os arquivos de configuração
+    copy_config_files(root_path_normalized)
 
     print('Estrutura de pastas criada com sucesso!')
 
